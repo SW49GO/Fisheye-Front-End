1d3263d85703ad0260fafcacbccdb24cc980f98d
SW49GO <gohard.so49@gmail.com>
2023-04-05 11:00:32 +0200

    Accueil CSS

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -11,17 +11,26 @@ function photographerFactory(data) {
   const picture = `assets/photographers/Photographers ID Photos/${portrait}`;
 
   /**
-   * Function to create HTLM Element (portraits and name of photographers)
+   * Function to create HTLM Elements (portraits and name of photographers)
    * @returns string (article)
    */
   function getUserCardDOM() {
+    console.log("factories/photographer.js->getUserCardDOM");
+    // Création des éléments HTML pour représenter la carte utilisateur
     const article = document.createElement("article");
-    const img = document.createElement("img");
-    img.setAttribute("src", picture);
-    const h2 = document.createElement("h2");
-    h2.textContent = name;
-    article.appendChild(img);
-    article.appendChild(h2);
+    article.setAttribute(
+      "aria-label",
+      "Carte d'identité du photographe " + name
+    );
+    article.innerHTML = `<a href="photographer.html?identify=${id}" aria-label="Lien vers la page du photographe ${name}">
+                            <img src="${picture}" alt="Portrait du photographe ${name}, lien vers sa page">
+                            <h2>${name}</h2>
+                        </a>
+                        <div class="photographer-description" aria-label="Localisation, slogan et tarifs du photographe ${name}">
+                          <h3>${city}, ${country}</h3> 
+                          <p>${tagline}</p>
+                          <p>${price}€/jour</p>
+                        </div>`;
     return article;
   }
   return { name, picture, getUserCardDOM };
